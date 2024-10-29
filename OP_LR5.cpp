47840ad7d3e14dde2ccc447e5f8b4da61bd57413
PavelKobchenko <pavelkobcenko@gmail.com>
2024-10-29 22:21:11 +0200

    Added file

---
+++ OP_LR5.cpp
@@ -0,0 +1,11 @@
+﻿// OP_LR5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    
+}
