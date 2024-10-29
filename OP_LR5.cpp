734cc8e2904e69bbbd216b63c159dd28f7f6ff74
PavelKobchenko <pavelkobcenko@gmail.com>
2024-10-29 22:33:00 +0200

    Removed useless comments

--- OP_LR5.cpp
+++ OP_LR5.cpp
@@ -1,7 +1,4 @@
-﻿// OP_LR5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <math.h>
 using namespace std;
 
